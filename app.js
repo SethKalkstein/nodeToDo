c72de3cf704940ac86deee94ea0089c4eb5611b5
SethKalkstein <sethkal7@yahoo.com>
2019-12-08 07:50:27 -0500

    started main view and defined get route in controller

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express;
+const app = express();
 const toDoController = require("./controllers/toDoController");
 
 //set up template engine to ejs 
