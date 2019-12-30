f4010cfe732f505e4bf034afe282874ad60e46f6
SethKalkstein <sethkal7@yahoo.com>
2019-12-30 18:01:26 -0500

    local dynamic add using jquery and ajax functional

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,14 +1,19 @@
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 
+const data = [{item: "get cat food"}, {item: "go to gym" }, {item: "make pickles"}, {item: "get sleep"}];
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = (app) => {
     
     app.get("/todo", (req, res)=>{
-        res.render("todo");
+        res.render("todo", {chores: data});
     });
 
-    app.post("/todo", (req, res)=>{
-
+    app.post("/todo", urlencodedParser, (req, res)=>{
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete("/todo", (req, res) =>{
